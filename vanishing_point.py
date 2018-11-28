8eeea168449f8757b2d5e21e6fafd0e549d439b9
王嘉楠 <jianan.wang@mall-ai.com>
2018-11-29 00:12:23 +0800

    v2

--- vanishing_point.py
+++ vanishing_point.py
@@ -20,19 +20,19 @@ def line_by_2points(pt1, pt2, image=None, vis=False):
         en2 = [None, image.shape[0], 1]#[None, image.shape[0]-1, 1]
         en1[0] = int(-1*np.dot(np.array(en1)[1:], line[1:])/line[0])
         en2[0] = int(-1*np.dot(np.array(en2)[1:], line[1:])/line[0])
-        cv2.line(image, tuple(en1[:2]), tuple(en2[:2]), (0,255,0), 10)
+        cv2.line(image, tuple(en1[:2]), tuple(en2[:2]), (0,255,0), 1)
     return line 
 
 cv2.namedWindow("image", 0)
 cv2.resizeWindow("image", 640, 480)
 cv2.setMouseCallback('image', select_point)  # 设置回调函数
-image = cv2.imread("box.jpg")
+image = cv2.imread("box_small.jpg")
 
 lines = []
 vanishing_points = []
 while True:
     for p in points:
-        cv2.circle(image, p, 30, (255,0,0), -1)
+        cv2.circle(image, p, 2, (255,0,0), -1)
     for i in range(0,len(points)-1,2):
         e1 = points[i]
         e2 = points[i+1]
@@ -40,11 +40,15 @@ while True:
         if line.tolist() in [it.tolist() for it in lines]:
             continue
         else:
+            print("line:", line)
             lines.append(line)
     for i in range(0, len(lines)-1, 2):
         vp = np.cross(lines[i], lines[i+1])
-        #vanishing_points.append(vp)
-        print("vp %d:"%(i/2), vp)
+        if vp.tolist() in [it.tolist() for it in vanishing_points]:
+            continue
+        else:
+            vanishing_points.append(vp)
+            print("vp %d:"%(i/2), vp)
     cv2.imshow("image", image)
     key = cv2.waitKey(1)
     if key == ord('q'):
